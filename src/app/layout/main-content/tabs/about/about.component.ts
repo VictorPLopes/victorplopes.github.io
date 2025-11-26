bdd9c94d56cc7685deccae22276e9465dfeae6ac
Victor Probio Lopes <victor.probio@gmail.com>
2025-11-26 17:30:08 -0300

    Remove unused import

--- src/app/layout/main-content/tabs/about/about.component.ts
+++ src/app/layout/main-content/tabs/about/about.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { MatDivider } from '@angular/material/divider';
 import { MatList, MatListItem } from '@angular/material/list';
 import { provideTranslocoScope, TranslocoPipe } from '@jsverse/transloco';
-import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'pf-about',
-  imports: [MatDivider, MatList, MatListItem, TranslocoPipe, MatIcon],
+  imports: [MatDivider, MatList, MatListItem, TranslocoPipe],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   providers: [provideTranslocoScope('about')],
