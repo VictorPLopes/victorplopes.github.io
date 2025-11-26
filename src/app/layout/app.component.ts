17559b6c6713e4b464a2d63b282fde1a27450d4f
Victor Probio Lopes <victor.probio@gmail.com>
2025-11-26 16:25:16 -0300

    Update typography

--- src/app/layout/app.component.ts
+++ src/app/layout/app.component.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { Profile } from './profile-sidebar/models/profile';
 import { Subscription } from 'rxjs';
 import { OptionsComponent } from '../options/options.component';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'pf-root',
@@ -28,10 +29,15 @@ export class AppComponent implements OnDestroy {
 
   private sub = new Subscription();
 
-  constructor(private translocoService: TranslocoService) {
+  constructor(private title: Title, private translocoService: TranslocoService) {
     this.sub.add(
       this.translocoService.selectTranslation('profile').subscribe((profile: any) => {
         this.myProfile = profile;
+      })
+    );
+    this.sub.add(
+      this.translocoService.selectTranslate('portfolio').subscribe((value: any) => {
+        this.title.setTitle(value);
       })
     );
   }
--- src/app/layout/main-content/tabs/about/about.component.ts
+++ src/app/layout/main-content/tabs/about/about.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatDivider } from '@angular/material/divider';
 import { MatList, MatListItem } from '@angular/material/list';
 import { provideTranslocoScope, TranslocoPipe } from '@jsverse/transloco';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'pf-about',
-  imports: [MatDivider, MatList, MatListItem, TranslocoPipe],
+  imports: [MatDivider, MatList, MatListItem, TranslocoPipe, MatIcon],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   providers: [provideTranslocoScope('about')],
