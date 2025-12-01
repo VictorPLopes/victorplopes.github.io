f38e7cbb885e0988fdf1b51a1184aef1a7d2a58a
Victor Probio Lopes <victor.probio@gmail.com>
2025-12-01 15:46:24 -0300

    Automatically adjust media content to fit in the available area

--- src/app/layout/main-content/main-content.component.ts
+++ src/app/layout/main-content/main-content.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TitleCasePipe } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule } from '@angular/router';
 import { TranslocoPipe } from '@jsverse/transloco';
