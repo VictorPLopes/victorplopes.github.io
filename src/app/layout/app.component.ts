a4a3456c3ce9a1ffa490611deb4af7daaadf4677
Victor Probio Lopes <victor.probio@gmail.com>
2025-12-08 08:59:53 -0300

    Readd loading spinner

--- src/app/layout/app.component.ts
+++ src/app/layout/app.component.ts
@@ -5,7 +5,18 @@ import { ProfileSidebarComponent } from './profile-sidebar/profile-sidebar.compo
 import { MainContentComponent } from './main-content/main-content.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { Profile } from './profile-sidebar/models/profile';
-import { filter, map, Observable, of, Subscription } from 'rxjs';
+import {
+  combineLatest,
+  filter,
+  map,
+  mapTo,
+  Observable,
+  of,
+  shareReplay,
+  startWith,
+  Subscription,
+  take,
+} from 'rxjs';
 import { OptionsComponent } from '../options/options.component';
 import { Title } from '@angular/platform-browser';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -37,6 +48,8 @@ import {
 })
 export class AppComponent implements OnInit, OnDestroy {
   loading$: Observable<boolean> = of(false);
+  hasLoadedOnce$: Observable<boolean> = of(false);
+  showInitialSpinner$: Observable<boolean> = of(false);
 
   myProfile!: Profile;
 
@@ -60,7 +73,7 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.loading$ = this.router.events.pipe(
+    const navEvents$ = this.router.events.pipe(
       filter(
         (e) =>
           e instanceof NavigationStart ||
@@ -68,7 +81,31 @@ export class AppComponent implements OnInit, OnDestroy {
           e instanceof NavigationCancel ||
           e instanceof NavigationError
       ),
-      map((e) => e instanceof NavigationStart)
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+
+    this.loading$ = navEvents$.pipe(
+      map((e) => e instanceof NavigationStart),
+      startWith(false),
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+
+    this.hasLoadedOnce$ = navEvents$.pipe(
+      filter(
+        (e) =>
+          e instanceof NavigationEnd ||
+          e instanceof NavigationCancel ||
+          e instanceof NavigationError
+      ),
+      take(1),
+      mapTo(true),
+      startWith(false),
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+
+    this.showInitialSpinner$ = combineLatest([this.loading$, this.hasLoadedOnce$]).pipe(
+      map(([loading, hasLoadedOnce]) => loading && !hasLoadedOnce),
+      shareReplay({ bufferSize: 1, refCount: true })
     );
   }
 
