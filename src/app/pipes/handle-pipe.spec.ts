f255270c7eb06355d86c883e34712fe637cbe852
VictorPLopes <victor.probio@gmail.com>
2026-07-31 23:08:28 -0300

    <feat>[portfolio]: <Delete spec files.>

--- src/app/pipes/handle-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HandlePipe } from './handle-pipe';
-
-describe('HandlePipe', () => {
-  it('create an instance', () => {
-    const pipe = new HandlePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
